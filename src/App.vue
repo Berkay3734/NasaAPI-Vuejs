87baaff897b9844e7a8254d40e35883ed3e9b249
Berkay3734 <berkay.gameofbilisim.com>
2023-03-14 21:14:49 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <WelcomePlanet v-if="showWelcomeScreen" />
+  <ListWrapper v-else />
+</template>
+
+<script>
+import ListWrapper from './components/ListWrapper.vue';
+import WelcomePlanet from './components/WelcomePlanet.vue';
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      showWelcomeScreen:false
+    }
+  },
+  created(){
+    setTimeout(()=>{
+        document.body.style.padding="20px"
+        document.body.style.overflow="auto"
+        this.showWelcomeScreen=false
+    },0)
+  },
+  components: {
+    WelcomePlanet,
+    ListWrapper
+}
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+::-webkit-scrollbar {
+  width: 4px;
+}
+::-webkit-scrollbar-track {
+  background: #0b3d91;
+}
+::-webkit-scrollbar-thumb {
+  background: #fc3d21;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #0b3d91;
+}
+
+body{
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  background-color: #0b3d91;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="card">
+        <img :src="item.links[0].href" />
+        <div class="container">
+        <h4>{{ item.data[0].title }}</h4>
+        <p>{{ item.data[0].description }}</p>
+        <div class="tags">
+        <div class="tag-item" v-for="tag in item.data[0]?.keywords?.slice(0,3)">
+                {{ tag }}
+        </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['item'],
+    name:"ListItem",
+}
+</script>
+<style scoped>
+.tags{
+    margin-bottom: 20px;
+    display: flex;
+    gap: 20px;
+    flex-wrap: wrap;
+    cursor: pointer;
+}
+.tag-item{
+    padding: 8px;
+    background-color: #ffff;
+    border: 1px solid;
+    font-size: 12px;
+    border-radius: 16px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    color: #280D08F9;
+}
+.card{
+    border: 2px solid #fc3d21;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+    transition: .5s;
+    width: 100%;
+    border-radius: 16px;
+}
+.card > img{
+    height: 300px;
+    object-fit: fill;
+    border-top-left-radius: 16px;
+    border-top-right-radius: 16px;
+    width: 100%;
+}
+h4{
+    color: #fc3d21;
+    font-weight: 500;
+}
+.card:hover{
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.8);
+    
+}
+.container{
+    padding: 4px 16px;
+}
+p{
+    font-size: 18px;
+    font-weight: 400;
+    color: #ccc;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+}
+</style>
